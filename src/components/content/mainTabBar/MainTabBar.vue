b8afc661ac02849d2ecf87b289ebbdc8cf3a159b
lavau <2723986904@qq.com>
2020-10-31 23:46:38 +0800

    2020/10/31 23：46

--- src/components/content/mainTabBar/MainTabBar.vue
+++ src/components/content/mainTabBar/MainTabBar.vue
@@ -1,11 +1,11 @@
 <template>
   <tab-bar>
-    <tab-bar-item path="/home" :active-color="'green'">
+    <tab-bar-item path="/home">
       <img slot="item-icon" src="@/assets/img/tabbar/home.svg" alt="">
       <img slot="item-icon-active" src="@/assets/img/tabbar/home_active.svg" alt="">
       <div slot="item-text">首页</div>
     </tab-bar-item>
-    <tab-bar-item path="/category" :active-color="'blue'">
+    <tab-bar-item path="/category">
       <img slot="item-icon" src="@/assets/img/tabbar/category.svg" alt="">
       <img slot="item-icon-active" src="@/assets/img/tabbar/category_active.svg" alt="">
       <div slot="item-text">分类</div>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,47 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :homebanners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+  </div>
 </template>
 
 <script>
+
+  import NavBar from "@/components/common/navbar/NavBar";
+  import {getHomeMultidata} from "@/network/home";
+  import HomeSwiper from "@/views/home/childComps/HomeSwiper";
+  import RecommendView from "@/views/home/childComps/RecommendView";
+
   export default {
-    name: "Home"
+    name: "Home",
+    components: {
+      NavBar,
+      HomeSwiper,
+      RecommendView
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: [],
+      };
+    },
+    created() {
+      // 1、请求多个数据
+      getHomeMultidata().then(res => {
+        // console.log(res);
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+        console.log(this.banners);
+        console.log(this.recommends);
+      })
+    }
   }
 </script>
 
 <style scoped>
-
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
